f7a0315052b414eb8d6723ca96fedfb7289a06c8
Dustranir <jorgerastelli92@gmail.com>
2025-05-23 21:38:40 -0300

    Ajuste de Templates e links do Aluno

--- aluno/urls.py
+++ aluno/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name='aluno'
+
 urlpatterns = [
     path('', views.aluno, name = "aluno"),  
     path('exibemensagem', views.exibe_mensagem, name="exibir_mensagem")  
